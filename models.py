2399fb7cb84565eab96ef1262756c15a864a0e98
Bashigea <bashigea@augsburg.edu>
2026-03-16 21:01:21 -0500

    Add initial scaffolds for ByteBites classes

--- models.py
+++ models.py
@@ -27,3 +27,44 @@
 - Can optionally reference the Customer who made the purchase.
 
 '''
+
+
+class Item:
+    def __init__(self, name: str, price: float, category: str, popularity_rating: float):
+        self.name = name
+        self.price = price
+        self.category = category
+        self.popularity_rating = popularity_rating
+
+
+class ItemCatalog:
+    def __init__(self):
+        self.items: list[Item] = []
+
+    def add_item(self, item: Item) -> None:
+        self.items.append(item)
+
+    def get_all_items(self) -> list[Item]:
+        return self.items
+
+    def filter_by_category(self, category: str) -> list[Item]:
+        return [item for item in self.items if item.category == category]
+
+
+class Transaction:
+    def __init__(self, items: list[Item], customer=None):
+        self.items = items
+        self.customer = customer
+
+    def compute_total(self) -> float:
+        return sum(item.price for item in self.items)
+
+
+class Customer:
+    def __init__(self, id: int, name: str):
+        self.id = id
+        self.name = name
+        self.purchase_history: list[Transaction] = []
+
+    def get_purchase_history(self) -> list[Transaction]:
+        return self.purchase_history
